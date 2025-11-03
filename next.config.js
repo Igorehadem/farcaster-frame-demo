b76d2a33c9a13d67850647d47a2985f192939a63
Igorehadem <140109468+Igorehadem@users.noreply.github.com>
2025-11-03 10:06:51 +0300

    fix(build): disable prerender for api routes

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // отключаем static export
+  experimental: {
+    appDir: false,
+  },
+};
+
+export default nextConfig;
