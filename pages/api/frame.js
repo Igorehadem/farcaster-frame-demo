d19540695c0922751db4466c0d1e097edd48b363
Igorehadem <140109468+Igorehadem@users.noreply.github.com>
2025-11-05 12:41:18 +0300

    fc:frame:post_url

--- pages/api/frame.js
+++ pages/api/frame.js
@@ -14,6 +14,7 @@ export default async function handler(req) {
       <meta name="robots" content="noindex,nofollow" />
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="${image}" />
+      <meta property="fc:frame:post_url" content="${tx}" />
       <meta property="fc:frame:button:1" content="Summon Base Tx" />
       <meta property="fc:frame:button:1:action" content="tx" />
       <meta property="fc:frame:button:1:target" content="${tx}" />
